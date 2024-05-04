a2bd818f09a8063a2df8686c5220324e5bd8ccfa
Zsolt Nagy <info@zsoltnagy.eu>
2024-05-04 18:08:00 +0200

    giphy feature

--- giphy.js
+++ giphy.js
@@ -3,7 +3,28 @@ const API_PREFIX = "https://api.giphy.com/v1/gifs/search?api_key=";
 const API_SETTINGS = "&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
 
 function renderGifs(response) {
-    console.log(response);
+    let result = "";
+
+    if (response.data.length === 0) {
+        renderError("No results.");
+    } else {
+        for (let meme of response.data) {
+            result += `
+                <img 
+                    src="${meme.images.original.url}" 
+                    alt="${meme.alt_text}" 
+                    class="meme-img" />
+            `;
+        }
+
+        document.querySelector(".js-memes-container").innerHTML = result;
+    }
+}
+
+function renderError(message) {
+    document.querySelector(".js-memes-container").innerHTML = `
+        <div class="alert alert-danger error-container">${message}</div>
+    `;
 }
 
 function getMemes(searchExpression) {
@@ -11,7 +32,8 @@ function getMemes(searchExpression) {
         `${API_PREFIX}${API_KEY}&q=${searchExpression}&limit=25${API_SETTINGS}`
     )
         .then((data) => data.json())
-        .then(renderGifs);
+        .then(renderGifs)
+        .catch(() => renderError("Error retrieving data."));
 }
 
 function formSubmitted(event) {
