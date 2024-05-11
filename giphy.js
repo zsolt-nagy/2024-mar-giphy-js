9cd88ba16550c4df63b9acd5c5ae37553ccabecb
Zsolt Nagy <info@zsoltnagy.eu>
2024-05-11 18:24:14 +0200

    pagination

--- giphy.js
+++ giphy.js
@@ -1,6 +1,119 @@
 const API_KEY = "hPLiLRR4wSIPRRFML70Qd8kOK5itmBhY";
 const API_PREFIX = "https://api.giphy.com/v1/gifs/search?api_key=";
-const API_SETTINGS = "&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+const API_SETTINGS = "&rating=g&lang=en&bundle=messaging_non_clips";
+
+/* Application internal state */
+let memesPerPage = 1;
+let currentPage = 1;
+let query = "";
+
+function generatePaginationListItems(pageCount) {
+    let startClass = "";
+    let endClass = "";
+    let previousClass = "";
+    let nextClass = "";
+
+    if (currentPage === 1) {
+        previousClass = "disabled";
+        startClass = "disabled";
+    }
+
+    if (pageCount === currentPage) {
+        endClass = "disabled";
+        nextClass = "disabled";
+    }
+
+    let innerPageLinks = "";
+    if (currentPage === 1) {
+        innerPageLinks = `
+            <li class="page-item disabled">
+                <a class="page-link" href="#" data-page="1">1</a>
+            </li>
+            <li class="page-item">
+                <a class="page-link" href="#" data-page="2">2</a>
+            </li>        
+        `;
+    } else if (currentPage === pageCount) {
+        innerPageLinks = `
+            <li class="page-item">
+                <a class="page-link" href="#" data-page="${currentPage - 1}">
+                    ${currentPage - 1}
+                </a>
+            </li>
+            <li class="page-item disabled">
+                <a class="page-link" href="#" data-page="${currentPage}">
+                    ${currentPage}
+                </a>
+            </li>        
+        `;
+    } else {
+        innerPageLinks = `
+            <li class="page-item">
+                <a class="page-link" href="#" data-page="${currentPage - 1}">
+                    ${currentPage - 1}
+                </a>
+            </li>
+            <li class="page-item disabled">
+                <a class="page-link" href="#" data-page="${currentPage}">
+                    ${currentPage}
+                </a>
+            </li>  
+            <li class="page-item">
+                <a class="page-link" href="#" data-page="${currentPage + 1}">
+                    ${currentPage + 1}
+                </a>
+            </li>      
+        `;
+    }
+
+    return `
+        <li class="page-item ${startClass}">
+            <a class="page-link" href="#" data-page="1">Start</a>
+        </li>
+        <li class="page-item ${previousClass}">
+            <a 
+                class="page-link" 
+                href="#" 
+                data-page="${currentPage - 1}">
+                    Previous
+            </a>
+        </li>
+        ${innerPageLinks}
+        <li class="page-item ${nextClass}">
+            <a 
+                class="page-link" 
+                href="#"
+                data-page="${currentPage + 1}">
+                    Next
+            </a>
+        </li>
+        <li class="page-item ${endClass}">
+            <a class="page-link" href="#" data-page="${pageCount}">
+                End
+            </a>
+        </li>    
+    `;
+}
+
+function renderPagination(paginationInfo) {
+    let { count, offset, total_count: totalCount } = paginationInfo;
+    let pageCount = Math.ceil(totalCount / count);
+
+    let html = `
+<nav 
+    aria-label="Page navigation example" 
+    data-bs-theme="dark"
+    class="pagination-component">
+    <ul class="pagination">
+        ${generatePaginationListItems(pageCount)}
+    </ul>
+    <span class="page-label">Page ${currentPage} of ${pageCount}</span>
+</nav>   
+    `;
+
+    document.querySelector(".js-pagination-top").innerHTML = html;
+    document.querySelector(".js-pagination-bottom").innerHTML = html;
+}
 
 function renderGifs(response) {
     let result = "";
@@ -18,6 +131,8 @@ function renderGifs(response) {
         }
 
         document.querySelector(".js-memes-container").innerHTML = result;
+
+        renderPagination(response.pagination);
     }
 }
 
@@ -27,9 +142,10 @@ function renderError(message) {
     `;
 }
 
-function getMemes(searchExpression, memeCount) {
+function getMemes() {
+    let offset = (currentPage - 1) * memesPerPage;
     fetch(
-        `${API_PREFIX}${API_KEY}&q=${searchExpression}&limit=${memeCount}${API_SETTINGS}`
+        `${API_PREFIX}${API_KEY}&q=${query}&limit=${memesPerPage}&offset=${offset}${API_SETTINGS}`
     )
         .then((data) => data.json())
         .then(renderGifs)
@@ -38,9 +154,24 @@ function getMemes(searchExpression, memeCount) {
 
 function formSubmitted(event) {
     event.preventDefault();
-    let inputFieldContent = document.querySelector("[name=meme-input]").value;
-    let memeCount = document.querySelector("[name=meme-count]").value;
-    getMemes(inputFieldContent, memeCount);
+    query = document.querySelector("[name=meme-input]").value;
+
+    /* set the global state */
+    memesPerPage = document.querySelector("[name=meme-count]").value;
+    currentPage = 1;
+
+    getMemes();
 }
 
 document.querySelector("#meme-form").addEventListener("submit", formSubmitted);
+
+function paginate(event) {
+    let page = event.target.dataset.page;
+    if (typeof page !== "undefined") {
+        event.preventDefault();
+        currentPage = Number(page);
+        getMemes();
+    }
+}
+
+document.querySelector("main").addEventListener("click", paginate);
